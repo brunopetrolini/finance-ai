ba5f47a06d1a47cf99b4729c150c9932909272e8
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-11-30 09:37:16 -0300

    feat: render translated values

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,11 +1,36 @@
-// 00:35:52
-
 "use client";
 
-import { Transaction, TransactionType } from "@prisma/client";
+import {
+  Transaction,
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 
-import { TransactionTypeBadge } from "../_components/type-bagde";
+import { TransactionTypeBadge } from "../_components/type-badge";
+
+const TRANSACTION_CATEGORY_LABELS = {
+  [TransactionCategory.EDUCATION]: "Educação",
+  [TransactionCategory.ENTERTAINMENT]: "Entretenimento",
+  [TransactionCategory.FOOD]: "Alimentação",
+  [TransactionCategory.HEALTH]: "Saúde",
+  [TransactionCategory.HOUSING]: "Moradia",
+  [TransactionCategory.OTHER]: "Outros",
+  [TransactionCategory.SALARY]: "Salário",
+  [TransactionCategory.TRANSPORTATION]: "Transporte",
+  [TransactionCategory.UTILITIES]: "Utilidades",
+};
+
+const TRANSACTION_PAYMENT_LABELS = {
+  [TransactionPaymentMethod.BANK_SLIP]: "Boleto",
+  [TransactionPaymentMethod.BANK_TRANSFER]: "Transferência",
+  [TransactionPaymentMethod.CASH]: "Dinheiro",
+  [TransactionPaymentMethod.CREDIT_CARD]: "Cartão de crédito",
+  [TransactionPaymentMethod.DEBIT_CARD]: "Cartão de débito",
+  [TransactionPaymentMethod.OTHER]: "Outros",
+  [TransactionPaymentMethod.PIX]: "Pix",
+};
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -15,23 +40,36 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "type",
     header: "Tipo",
-    cell: (row) => (
-      <TransactionTypeBadge
-        transactionType={row.getValue() as TransactionType}
-      />
-    ),
+    cell: (row) => {
+      const transactionType = row.getValue<TransactionType>();
+      return <TransactionTypeBadge transactionType={transactionType} />;
+    },
   },
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: (row) => {
+      const category = row.getValue<TransactionCategory>();
+      return TRANSACTION_CATEGORY_LABELS[category];
+    },
   },
   {
     accessorKey: "paymentMethod",
     header: "Método",
+    cell: (row) => {
+      const paymentMethod = row.getValue<TransactionPaymentMethod>();
+      return TRANSACTION_PAYMENT_LABELS[paymentMethod];
+    },
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: (row) => {
+      const date = row.getValue<Date>();
+      return new Date(date).toLocaleDateString("pt-BR", {
+        dateStyle: "long",
+      });
+    },
   },
   {
     accessorKey: "amount",
